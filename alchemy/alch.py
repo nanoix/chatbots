7de3d121bd5704e25cafe37d81bfe7fea7877d65
nanoix <64819306+nanoix@users.noreply.github.com>
2020-05-05 05:09:59 +0200

    Add files via upload

---
+++ alchemy/alch.py
@@ -0,0 +1,77 @@
+import random
+
+print("Hello Hakan")
+
+for i in range(99999999999999):
+	x = random.randint(0,100)
+	y = str(input())
+	if "fire" in y:
+		print("the anguishes of our time burns within the constraints")
+	if "coal" in y:
+		print("tastes good")
+	if "smoke" in y:
+		print("chill")
+	if "time" in y:
+		print("too late")
+	if "water" in y:
+		print("the watefall spreads out to lakes")
+	if "ice" in y:
+		print("cold isn't it?")		
+	if "air" in y:
+		print("the breeze is fine")
+	if "steam" in y:
+		print("the smell is funny")
+	if "dog" in y:
+		print("a better friend, you can't find")
+	if "cat" in y:
+		print("the ninja is outdoors")
+	if "way" in y:
+		print("arguments are indifferent")
+	if "you" in y:
+		print("better days are coming")
+	if "road" in y:
+		print("the cross is bleeding")
+	if "what" in y:
+		print("that is the question")
+	if "listen" in y:
+		print("the hills have eyes")
+	if "true" in y or "yes" in y:
+		print("yes, indeed")
+	if "indeed" in y:
+		print("perhaps so")
+	if "blue" in y:
+		print("the skies are unclear")
+	if "green" in y:
+		print("the trees are blossomed")
+	if "red" in y:
+		print("my blood shakes in this endeavor")
+	if "black" in y:
+		print("the shadow is lingering")
+	if "white" in y:
+		print("innocence lost")
+	if "col" in y:
+		print("the shades are imminent")
+	if "shadow" in y:
+		print("do not submit to the darkness")
+	if "darkness" in y:
+		print("you should run")
+	if "ok" in y:
+		print("good luck, traveler")
+	if "hello" in y or "hi" in y:
+		print("you again")
+	if "who" in y:
+		print("silence your jaw")	
+	if "sorry" in y:
+		print("sacrifices have to be made, not easily forgotten")
+	if "where" in y or "it" in y:
+		print("treasures hidden below the entrenches")
+	if "question" in y:
+		print("today isn't it")
+	if "today" in y:
+		print("exactly")
+	if "perhaps" in y:
+		print("the meadow is waiting for you")	
+	if "stop" in y or "bye" in y or "cya" in y:
+		print("we'll meet again later")
+		break
+
